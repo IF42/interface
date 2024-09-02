b06f8519af5de3d038473f873503f7a033fa2924
Petr Horáček <phoracek.edit@gmail.com>
2024-09-02 11:40:17 +0200

    added reset iterator function

--- src/vector/vector.h
+++ src/vector/vector.h
@@ -2,19 +2,22 @@
 #define _VECTOR_H_
 
 #include <stddef.h>
-#include <alloc/alloc.h>
 
 
-typedef struct vector{
+typedef struct vector {
+    void (*reset_iterator)(const struct vector*);
     void * (*next)(const struct vector *);
 } vector;
 
 
+#define vector_reset_iterator(T) ((T)->reset_iterator((T)))
+
 #define vector_next(T) ((T)->next((T)))
 
 
-#define fold(T, iter_var, init,  expr) \
-    init; for(void * iter_var = vector_next((T)); iter_var != NULL; iter_var = vector_next((T))) expr
+#define foreach(T, iter_var)     \
+    vector_reset_iterator((T));  \
+    for(void * iter_var = vector_next((T)); iter_var != NULL; iter_var = vector_next((T)))
 
 
 #endif
